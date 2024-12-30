7a00ba38b2e34a7fedfbbf4da33a5cb67cfb524d
nobrainghost <nobrainghost@nobrainghost.com>
2024-12-30 15:34:04 +0300

    add line draw using Bresenham's algorithm

--- reusabletools.py
+++ reusabletools.py
@@ -1,4 +1,3 @@
-# Simulating a Cartesian Plane
 def create_canvas(width, height):
     return [[' ' for _ in range(width)] for _ in range(height)]
 
@@ -9,26 +8,42 @@ def print_canvas(canvas):
 def draw_point(canvas, x, y):
     center_x = len(canvas) // 2
     center_y = len(canvas[0]) // 2
-    canvas[center_x - x][center_y + y] = '*'
+    canvas_x = center_x - x
+    canvas_y = center_y + y
+    if 0 <= canvas_x < len(canvas) and 0 <= canvas_y < len(canvas[0]):
+        canvas[canvas_x][canvas_y] = '*'
 
 def add_axis(canvas):
     center_x = len(canvas) // 2
     center_y = len(canvas[0]) // 2
-
     for i in range(len(canvas)):
         canvas[i][center_y] = '|'
-
     for i in range(len(canvas[0])):
         canvas[center_x][i] = '-'
     canvas[center_x][center_y] = '0'
 
-canvas = create_canvas(11, 11)  # Create a canvas with odd dimensions
-add_axis(canvas)
+def draw_line(canvas, x1, y1, x2, y2):
+    dx = abs(x2 - x1)
+    dy = abs(y2 - y1)
+    sx = 1 if x1 < x2 else -1
+    sy = 1 if y1 < y2 else -1
+    err = dx - dy
+    
+    while True:
+        draw_point(canvas, x1, y1)
+        
+        if x1 == x2 and y1 == y2:
+            break
+        
+        e2 = 2 * err
+        if e2 > -dy:
+            err -= dy
+            x1 += sx
+        if e2 < dx:
+            err += dx
+            y1 += sy
 
-# Draw points
-draw_point(canvas, 0, 5)  # Point at (0, 5)
-draw_point(canvas, 2, -3)  # Point at (2, -3)
-draw_point(canvas, -4, -2)  # Point at (-4, -2)
-
-# Print the canvas
-print_canvas(canvas)
+# canvas = create_canvas(11, 11)
+# add_axis(canvas)
+# draw_line(canvas, 0, 0, 10, 10)
+# print_canvas(canvas)
